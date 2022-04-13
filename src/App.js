fbdd421f9b23f431481c059fa4a305f477e8b9ca
baolongt <tranbaolong14@gmail.com>
2022-04-13 16:17:00 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Web3 from "web3";
+import { useEffect } from "react";
+import loadContract from "./loadContract";
+
+async function loadWeb3() {
+	if (window.ethereum) {
+		window.web3 = new Web3(window.ethereum);
+		window.ethereum.enable();
+	}
+}
+
+async function load() {
+	await loadWeb3();
+	window.contract = await loadContract();
+	updateStatus("Ready!");
+}
+
+function updateStatus(status) {
+	const statusEl = document.getElementById("status");
+	statusEl.innerHTML = status;
+	console.log(status);
+}
+
+async function callFucnMint() {
+	updateStatus("call func");
+	try {
+		const account = await getCurrentAccount();
+		const result = await window.contract.methods
+			.mint(1)
+			.send({ from: account });
+		console.log(result);
+	} catch (e) {
+		console.error(e);
+	}
+	updateStatus("done");
+}
+
+async function getCurrentAccount() {
+	const accounts = await window.web3.eth.getAccounts();
+	return accounts[0];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		load();
+	}, []);
+	return (
+		<div className="App">
+			Status: <span id="status">Loading...</span>
+			<div>
+				<button onClick={() => callFucnMint()}>Mint</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
